df25251ed99c4f872baf44cda35837dff3ad284d
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-11 15:25:58 -0500

    added feedings and meal form

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 from django.http import HttpResponse
 
 def home(request):
@@ -12,7 +13,15 @@ def dogs_index(request):
     return render(request, 'dogs/index.html', { 'dogs': dogs })
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  feeding_form = FeedingForm()
   return render(request, 'dogs/detail.html', { 'dog': dog })
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
     model = Dog
