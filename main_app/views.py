8ad0008cf76984d7872450095d2e4493eb55550a
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-11 14:37:00 -0500

    Added edit and delete button

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from django.http import HttpResponse
 
@@ -14,6 +14,15 @@ def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', { 'dog': dog })
 
-class DogCreate(CreateView)
+class DogCreate(CreateView):
     model = Dog
     fields = '__all__'
+    success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
