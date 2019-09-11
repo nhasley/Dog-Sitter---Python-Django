b1c8f58e1b19f0629dad614d706c526ea8f5da60
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-11 12:15:25 -0500

    models, views updated. added all dogs page

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+class Dog:  
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Norman', 'dalmation', 'Active', 3),
+  Dog('David', 'husky', 'Good at Fetch', 0),
+  Dog('Bobbi', 'corgi', 'Fluffy dog', 4)
+]
+
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+def about(request):
+  return render(request, 'about.html')
+def dogs_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
