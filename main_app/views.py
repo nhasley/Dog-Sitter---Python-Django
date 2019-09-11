77b8bdafb584ef98599afed134b02d94cfc0ed3c
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-11 14:30:06 -0500

    connected admin to database. Added dog_form and edited html

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from .models import Dog
 from django.http import HttpResponse
 
-class Dog:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Norman', 'dalmation', 'Active', 3),
-  Dog('David', 'husky', 'Good at Fetch', 0),
-  Dog('Bobbi', 'corgi', 'Fluffy dog', 4)
-]
-
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Test</h1>')
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 def dogs_index(request):
-  return render(request, 'dogs/index.html', { 'dogs': dogs })
+    dogs = Dog.objects.all()
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView)
+    model = Dog
+    fields = '__all__'
