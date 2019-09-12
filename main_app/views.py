224a143409da37f2e74abb5fcab295c0e7de7706
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-12 14:35:16 -0500

    Created Photo model and implemented in html

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Dog, Toy
+import uuid
+import boto3
+from .models import Dog, Toy, Photo
 from .forms import FeedingForm
 from django.http import HttpResponse
+
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET = 'dogcollec'
 
 def home(request):
     return HttpResponse('<h1>Test</h1>')
@@ -30,6 +35,20 @@ def add_feeding(request, dog_id):
 def assoc_toy(request, dog_id, toy_id):
   Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('detail', dog_id=dog_id)
+def add_photo(request, dog_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url, dog_id=dog_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', dog_id=dog_id)
+
 
 class DogCreate(CreateView):
     model = Dog
