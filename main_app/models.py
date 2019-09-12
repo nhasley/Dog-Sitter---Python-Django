af062683d812d1d812197e79fa58bf5116a8be5e
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-12 10:01:14 -0500

    Adding Toy Model

--- main_app/models.py
+++ main_app/models.py
@@ -8,19 +8,28 @@ MEALS = (
     ('D', 'Dinner')
 )
 
-# Create your models here.
-class Dog(models.Model):  
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
 
   def __str__(self):
-        return self.name
+    return self.name
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
-  
+
   def fed_for_today(self):
     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
@@ -34,3 +43,6 @@ class Feeding(models.Model):
   dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
   def __str__(self):
     return f'{self.get_meal_display()} on {self.date}'
+
+  class Meta:
+    ordering = ['-date']
