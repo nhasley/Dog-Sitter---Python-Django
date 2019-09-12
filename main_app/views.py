6538e55cd9d2cc50a166a63be86b32113a73918d
Nova Hasley <supernova@Novas-MacBook-Pro.local>
2019-09-12 10:59:47 -0500

    Added toys html and views/urls/models code

--- main_app/views.py
+++ main_app/views.py
@@ -14,9 +14,11 @@ def dogs_index(request):
     return render(request, 'dogs/index.html', { 'dogs': dogs })
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'dogs/detail.html', {
-    'dog': dog, 'feeding_form': feeding_form
+    'dog': dog, 'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have
   })
 def add_feeding(request, dog_id):
   form = FeedingForm(request.POST)
@@ -24,6 +26,9 @@ def add_feeding(request, dog_id):
     new_feeding = form.save(commit=False)
     new_feeding.dog_id = dog_id
     new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
   return redirect('detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
