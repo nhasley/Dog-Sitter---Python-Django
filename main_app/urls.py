627618e0f95eb7166bea056216ad1dc089a85dc2
Nova Hasley <supernova@Novas-MBP.lan1>
2019-09-15 10:08:33 -0500

    oauth added to urls and models. login html added

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -18,4 +18,5 @@ urlpatterns = [
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
   path('dogs/<int:dog_id>/add_photo/', views.add_photo, name='add_photo'),
+  path('accounts/', include('django.contrib.auth.urls')),
 ]
